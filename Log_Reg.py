1376a1566c9aadce158f4de77f499d86b8a1c5f4
Andrea Viviani <andrea.viviani89@gmail.com>
2022-04-08 16:32:38 +0200

    Update Log_Reg.py

--- Log_Reg.py
+++ Log_Reg.py
@@ -9,10 +9,9 @@ class LogReg:
     # x = None
     # y = None
 
-    def __init__(self, z, a, b):
-        self.z = z
-        self.a = a
-        self.b = b
+    def __init__(self, w, b):
+        self.w=w
+        self.b=b
 
     def __sigmoid(self, z):
         '''The range of inputs is the set of all Real Numbers and the range of outputs is between 0 and 1.
@@ -25,9 +24,18 @@ class LogReg:
 
 
 
-    def __cost_function(self, y):
-        y_pred = self.sig_func
-        cost = -(1/X.shape[0])*np.sum(Y*np.log(y_pred) + (1-y)*np.log(1-y_pred))
+    def __cost_function(self, y, w, b, X, Y):
+        # y_pred = self.sig_func
+        # cost = -(1/X.shape[0])*np.sum(Y*np.log(y_pred) + (1-y)*np.log(1-y_pred))
+        # return cost
+
+        #   m --> number of training examples
+        m = X.shape[0]
+
+        final_result = self.sigmoid(np.dot(w,X.T)+b)
+        Y_T = Y.T
+        cost = (-1/m)*(np.sum((Y_T*np.log(final_result)) + ((1-Y_T)*(np.log(1-final_result)))))
+        
         return cost
 
 
@@ -37,3 +45,15 @@ class LogReg:
         Inverse_DotXT = np.dot(Dotx_inverse,X.T)
         final = np.dot(Inverse_DotXT, y)
         return final
+
+
+    def model_train(self, X, Y, w, learning_rate, iters):
+        
+        """ Model training """
+        cost_history = []
+
+        for i in range(iters):
+            w = self.gradient_cost(X, Y, w, learning_rate)
+
+            cost = self.cost_function(X, Y, w)
+            cost_history.append(cost)
